d3f2d411ffd956410546f2b113e1d90e4ab495d2
Aiden Harrington <aidenkharrington@gmail.com>
2021-06-12 18:36:42 -0700

    app completed

---
+++ lib/helpers/location_helper.dart
@@ -0,0 +1,9 @@
+const GOOGLE_API_KEY = 'AIzaSyBNZDZHdx_ydxL8VydEMOJgBxWPFo7SfCc';
+
+class LocationHelper {
+  static String generateLocationPreviewImage(
+      {double latitude, double longitude}) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+    //return 'https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=$GOOGLE_API_KEY';
+  }
+}
--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 
 import '../providers/great_places.dart';
 import '../widgets/image_input.dart';
+import '../widgets/location_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -50,6 +51,10 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     ),
                     SizedBox(height: 10),
                     ImageInput(_selectImage),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LocationInput(),
                   ],
                 ),
               ),
